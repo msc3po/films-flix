0ddba86e0dd28c54cf5aff1aab4160ba74f92ab6
Manel Soler <manelsoler@gmail.com>
2023-11-20 17:25:38 +0100

    dataForm for recover data from form

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import './App.css'
+import { useRef } from 'react'
 import { Movies } from './components/movies.jsx'
 import { useMovies } from './hooks.js/useMovies.js'
 
 function App () {
-  const { movies: mappedMovies } = useMovies()
+  const { movies } = useMovies()
+  const inputRef = useRef()
+
+  const handleSubmmit = (event) => {
+    event.preventDefault()
+    const value = inputRef.current.value
+    console.log(value)
+  }
+
   return (
 
     <div className='page'>
 
       <header>
         <h1> Films Searcher</h1>
-        <form className='form' name='searchForm' id='searchForm'>
-          <input type='text' placeholder='Star Wars, Star trek...' />
+        <form className='form' onSubmit={handleSubmmit}>
+          <input ref={inputRef} type='text' placeholder='Star Wars, Star trek...' />
           <button type='submit'>Search</button>
         </form>
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
 
     </div>
