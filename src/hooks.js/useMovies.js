511683448fe87bde45668252da7cea96cff98f34
Manel Soler <manelsoler@gmail.com>
2023-11-24 18:59:50 +0100

    previous search contention

--- src/hooks.js/useMovies.js
+++ src/hooks.js/useMovies.js
@@ -1,15 +1,17 @@
 import { searchMovies } from '../services/movies'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 export function useMovies ({ search }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState([false])
   const [error, setError] = useState([null])
+  const previoiusSerch = useRef(search)
 
   const getMovies = async () => {
     try {
       setLoading(true)
       setError(null)
+      previoiusSerch.current = search
       const newMovies = await searchMovies({ search })
       setMovies(newMovies)
     } catch (e) {
