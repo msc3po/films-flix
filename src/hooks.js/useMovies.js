60654f1cd7f4069e5188bf5b45ed13355610154a
Manel Soler <manelsoler@gmail.com>
2023-11-24 17:31:43 +0100

    fetch data from api

--- src/hooks.js/useMovies.js
+++ src/hooks.js/useMovies.js
@@ -1,7 +1,9 @@
-import responseMovies from '../mocks/with-results.json'
+import withResults from '../mocks/with-results.json'
 import withoutResults from '../mocks/with-no-results.json'
+import { useState } from 'react'
 
-export function useMovies () {
+export function useMovies ({ search }) {
+  const [responseMovies, setResponseMovies] = useState([])
   const movies = responseMovies.Search
   const mappedMovies = movies?.map((movie) => ({
     id: movie.imdbID,
@@ -9,5 +11,19 @@ export function useMovies () {
     year: movie.Year,
     poster: movie.Poster
   }))
-  return { movies: mappedMovies }
+
+  const getMovies = () => {
+    if (search) {
+      //   setResponseMovies(withResults)
+      fetch(`https://www.omdbapi.com/?apikey=4287ad07&s=${Search}`)
+        .then((response) => response.json())
+        .then((dataJsonObject) => {
+          setResponseMovies(dataJsonObject)
+        })
+    } else {
+      setResponseMovies(withoutResults)
+    }
+  }
+
+  return { movies: mappedMovies, getMovies }
 }
