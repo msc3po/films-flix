5bbd63a0682b4e20609890e4385ca475d790be98
Manel Soler <manelsoler@gmail.com>
2023-12-04 16:35:05 +0100

    extract fetch logic to movie service

--- src/hooks.js/useMovies.js
+++ src/hooks.js/useMovies.js
@@ -1,25 +1,12 @@
-import withResults from '../mocks/search_Result.json'
 import { useState } from 'react'
-import withoutResults from '../mocks/no_Results.json'
+import { searchMovies } from '../services/movies'
 
-const URL_fixed = 'http://www.omdbapi.com/?apikey=4287ad07&s=avengers'
+export function useMovies ({ search }) {
+  const [movies, setMovies] = useState([])
 
-export function useMovies (search) {
-  const [responseMovies, setResponseMovies] = useState([])
-  const movies = responseMovies.Search // where the movies are.
-  const mappedMovies = movies?.map((movie) => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster
-  }))
-
-  const getMovies = () => {
-    if (search) {
-      setResponseMovies(withResults)
-    } else {
-      setResponseMovies(withoutResults)
-    }
+  const getMovies = async () => {
+    const newMovies = await searchMovies({ search })
+    setMovies(newMovies)
   }
-  return { movies: mappedMovies, getMovies }
+  return { movies, getMovies }
 }
